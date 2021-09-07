79ffb59d06c34eef03840ade1d6f0d9be2e38f7b
ANJALI WEERASINGHE <sapnaweerasinghe98@gmail.com>
2021-09-07 21:25:29 +0530

    Add cart, delivery and payment form

---
+++ src/Components/MainPages/Cart/Cart.js
@@ -0,0 +1,129 @@
+import React, {useContext, useState, useEffect} from 'react'
+import {GlobalState} from '../../../GlobalState'
+import axios from 'axios'
+import {Card, Form, Button, Container} from 'react-bootstrap';
+import {useHistory, useParams} from 'react-router-dom'
+
+
+function Cart() {
+    const state = useContext(GlobalState)
+    const [cart, setCart] = state.userAPI.cart
+    const [token] = state.token
+    const [total, setTotal] = useState(0)
+    const history = useHistory()
+
+    useEffect(() =>{
+        const getTotal = () =>{
+            const total = cart.reduce((prev, item) => {
+                return prev + (item.price * item.quantity)
+            },0)
+
+            setTotal(total)
+        }
+
+        getTotal()
+
+    },[cart])
+
+    const addToCart = async (cart) =>{
+        await axios.patch('/user/addcart', {cart}, {
+            headers: {Authorization: token}
+        })
+    }
+
+
+    const increment = (id) =>{
+        cart.forEach(item => {
+            if(item._id === id){
+                item.quantity += 1
+            }
+        })
+
+        setCart([...cart])
+        addToCart(cart)
+    }
+
+    const decrement = (id) =>{
+        cart.forEach(item => {
+            if(item._id === id){
+                item.quantity === 1 ? item.quantity = 1 : item.quantity -= 1
+            }
+        })
+
+        setCart([...cart])
+        addToCart(cart)
+    }
+
+    const removeProduct = id =>{
+        if(window.confirm("Do you want to delete this product?")){
+            cart.forEach((item, index) => {
+                if(item._id === id){
+                    cart.splice(index, 1)
+                }
+            })
+
+            setCart([...cart])
+            addToCart(cart)
+        }
+    }
+
+    const addPaymentDetails = async () => {
+        
+        //const valid  = 
+       
+        //if (valid) {
+
+            setCart([])
+            addToCart([])
+            const payment = history.push('/addpayment/');
+            //const {city, postal_code, address, contactNo, cardType, cardNo, expMonth, expYear, cvn ,cart} = payment
+             axios.post('/api/payment', {cart}, {
+                headers: {Authorization: token}
+             })
+            
+            //alert("You have successfully placed an order.")
+        //}
+    }
+
+
+    if(cart.length === 0) 
+        return <h2 style={{textAlign: "center", fontSize: "5rem"}}>Cart Empty</h2> 
+
+    return (
+        <div>
+            {
+                cart.map(product => (
+                    <div className="detail cart" key={product._id}>
+                        <img src={product.images.url} alt="" />
+
+                        <div className="box-detail">
+                            <h2>{product.title}</h2>
+
+                            <h3>LKR {product.price * product.quantity}</h3>
+                            <p>{product.description}</p>
+                            <p>{product.content}</p>
+
+                            <div className="amount">
+                                <button onClick={() => decrement(product._id)}> - </button>
+                                <span>{product.quantity}</span>
+                                <button onClick={() => increment(product._id)}> + </button>
+                            </div>
+                            
+                            <div className="delete" 
+                            onClick={() => removeProduct(product._id)}>
+                                X
+                            </div>
+                        </div>
+                    </div>
+                ))
+            }
+
+            <div className="total">
+                <h3>Total: LKR {total}</h3>
+                <Button variant="primary" style={{background: "#28c1e4", width:110}}  onClick = { () => addPaymentDetails()} >Check Out</Button>
+            </div>
+        </div>
+    )
+}
+
+export default Cart
