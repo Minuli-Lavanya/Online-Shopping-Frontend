5fcee02e786cb2fd38c3ba3ce5d2a0fc4dc52cb6
ANJALI WEERASINGHE <sapnaweerasinghe98@gmail.com>
2021-09-07 21:26:41 +0530

    Add history details

---
+++ src/Components/MainPages/History/OrderHistory2.js
@@ -0,0 +1,70 @@
+import React, {useContext, useEffect} from 'react'
+import {GlobalState} from '../../../GlobalState'
+import {Link} from 'react-router-dom'
+import axios from 'axios'
+import {Card, Form, Button, Container} from 'react-bootstrap';
+
+function OrderHistory2() {
+    const state = useContext(GlobalState)
+    const [history, setHistory] = state.userAPI.history
+    const [isAdmin] = state.userAPI.isAdmin
+    const [token] = state.token
+    
+
+    useEffect(() => {
+        if(token){
+            const getHistory = async() =>{
+                if(isAdmin){
+                    const res = await axios.get('/api/payment', {
+                        headers: {Authorization: token}
+                    })
+                    setHistory(res.data)
+                }else{
+                    const res = await axios.get('/user/history', {
+                        headers: {Authorization: token}
+                    })
+                    setHistory(res.data)
+                }
+            }
+            getHistory()
+        }
+    },[token, isAdmin, setHistory])
+
+    return (
+        <div className="history-page">
+            <h2>My Order History</h2>
+            
+            <h4>You have {history.length} ordered</h4>
+            {/* <Button variant="primary" type="submit" style={{marginLeft:1000,marginTop:-90, background: "#24547c", width:150}}>Generate Report</Button> */}
+            <table>
+                <thead>
+                    <tr>
+                        <th>Payment ID</th>
+                        <th>Date of Purchased</th>
+                        <th>Delivery Status</th>
+                        <th>View</th>
+                        <th>Actions</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        history.map(items => (
+                            <tr key={items._id}>
+                                <td>{items._id}</td>
+                                <td>{new Date(items.createdAt).toLocaleDateString()}</td>
+                                <td><Link to={`/history/${items._id}`}>View</Link></td>
+                                {/* <td><Button variant="primary" type="submit" style={{marginLeft:250, background: "#24547c", width:70}}>UPDATE</Button></td>
+                                <td><Button variant="primary" type="submit" style={{marginLeft:250, background: "#24547c", width:70}}>DELETE</Button></td> */}
+
+
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default OrderHistory2
