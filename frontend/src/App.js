18acea1452e65158e791bd8fb0a2755f8615a7db
Minuli-Lavanya <minulilavanya@gmail.com>
2021-09-09 08:59:01 +0530

    dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import ItemList from './Components/ItemList';
 import ViewItem from './Components/ViewItem';
 import HeaderComponent from './Components/HeaderComponent';
 import SideBar from './Components/SideBar';
-import Dashboard from './Components/Deashboard';
+import Dashboard from './Components/Dashboard';
 import Login from './Components/Login';
 import Register from './Components/Register';
 
@@ -21,14 +21,14 @@ function App() {
         
         <HeaderComponent/>
         <SideBar/>
-        <Route path="/" exact component={SupplierDetailsList}/>
+        <Route path="/" exact component={SupplierDetailsList}/> 
         <Route path="/add" exact component={SupplierDetails}/>
         <Route path="/itemadd" exact component={ItemDetails}/>
         <Route path ="/itemList" exact component={ItemList}/>
         <Route path ="/itemList/:userId" exact component={ViewItem}/>
         <Route path ="/login" exact component={Login}/>
         <Route path ="/Register" exact component={Register}/>
-        {/* <Route path ="/Dashboard" exact component={Dashboard}/> */}
+        <Route path ="/Dashboard" exact component={Dashboard}/> 
         
       </div>
       
