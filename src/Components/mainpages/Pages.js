4bb8a36bc2e305466c605d73aaf9e3faf94a1fac
Minuli-Lavanya <minulilavanya@gmail.com>
2021-09-30 12:43:52 +0530

    add page.js

--- src/Components/mainpages/Pages.js
+++ src/Components/mainpages/Pages.js
@@ -1,21 +1,20 @@
 import React, {useContext} from 'react'
 import {Switch, Route} from 'react-router-dom'
-import Products from './products/Products'
-import DetailProduct from './detailProduct/DetailProduct'
-import Login from './auth/Login'
-import Register from './auth/Register'
-// import OrderHistory from './history/OrderHistory'
-// import OrderDetails from './history/OrderDetails'
-// import Cart from './cart/Cart'
-import NotFound from './utils/not_found/NotFound'
-import Categories from './categories/Categories'
-import CreateProduct from './createProduct/CreateProduct'
-
-import Summary from './productSummary/Summary'
+//import Products from './products/Products'
+//import DetailProduct from './detailProduct/DetailProduct'
+//import Login from './auth/Login'
+//import Register from './auth/Register'
+import OrderHistory from './History/OrderHistory'
+import OrderHistory2 from './History/OrderHistory2'
+import OrderDetails from './History/OrderDetails'
+import Cart from './Cart/Cart'
+import NotFound from './Utils/not_found/NotFound'
+//import Categories from './categories/Categories'
+//import CreateProduct from './createProduct/CreateProduct'
+import Paymentdetails from './Cart/Paymentdetails'
+import PaymentDetails1 from './Cart/PaymentDetails1'
 
 import {GlobalState} from '../../GlobalState'
-
-import Home from '../mainpages/home/home1'
 
 
 function Pages() {
@@ -26,10 +25,7 @@ function Pages() {
 
     return (
         <Switch>
-
-            <Route path="/" exact component={Home} />
-
-            <Route path="/product" exact component={Products} />
+            {/* <Route path="/" exact component={Products} />
             <Route path="/detail/:id" exact component={DetailProduct} />
 
             <Route path="/login" exact component={isLogged ? NotFound : Login} />
@@ -37,14 +33,15 @@ function Pages() {
 
             <Route path="/category" exact component={isAdmin ? Categories : NotFound} />
             <Route path="/create_product" exact component={isAdmin ? CreateProduct : NotFound} />
-            <Route path="/edit_product/:id" exact component={isAdmin ? CreateProduct : NotFound} />
+            <Route path="/edit_product/:id" exact component={isAdmin ? CreateProduct : NotFound} /> */}
 
-            <Route path="/summary" exact component={isAdmin ? Summary : NotFound} />
-
-            {/* <Route path="/history" exact component={isLogged ? OrderHistory : NotFound} />
+            <Route path="/history" exact component={isLogged ? OrderHistory : NotFound} />
+            <Route path="/history1" exact component={isLogged ? OrderHistory2 : NotFound} />
             <Route path="/history/:id" exact component={isLogged ? OrderDetails : NotFound} />
 
-            <Route path="/cart" exact component={Cart} /> */}
+            <Route path="/cart" exact component={Cart} />
+            <Route path="/addpayment" exact component={Paymentdetails} />
+            <Route path="/addpayment2" exact component={PaymentDetails1} />
 
 
             <Route path="*" exact component={NotFound} />
